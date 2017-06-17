fc35a86182a7d982128c1e36541f0e51d53f1e2e
Alberto <albertoandreotti@gmail.com>
2017-06-17 12:55:29 -0300

    Renaming of main class and some corrections in data

--- src/main/scala/me/jpaa/SparkStreaming.scala
+++ src/main/scala/me/jpaa/SparkStreaming.scala
@@ -3,6 +3,7 @@ package me.jpaa;
 
 import org.apache.spark.sql.streaming.ProcessingTime
 import org.apache.spark.sql.{SQLContext, SparkSession}
+import java.io.File
 
 /**
  * Created by jose on 21/10/16.
@@ -20,30 +21,30 @@ object SparkStreaming {
 
       import spark.implicits._
 
-      val filename = "/home/jose/spark_stream"
+      val fullPath = this.getClass.getResource("/people1.json").getPath
+      val resourcesPath = (new File(fullPath)).getParent
 
-      val firstFile = spark.read.json(s"$filename/*.json")
+      // read the first file to get the schema
+      val firstFile = spark.read.json(s"$fullPath")
 
       val rawRecords = spark.readStream
         .schema(firstFile.schema)
-        .json(s"$filename/*.json")
+        .json(s"$resourcesPath/*.json")
 
       val ageEvents = rawRecords
         .select($"name",  - $"age" + 2017 as "birthyear")
 
-      val streamingETLQuery = ageEvents
+      val streamingQuery = ageEvents
         .writeStream
         // check for files every 2s
         .trigger(ProcessingTime("2 seconds"))
         // write in the console
         .format("console")
-        .option("path", s"$filename/birthyears")
+        .option("path", s"$resourcesPath/birthyears")
         .start()
 
-      // Wait 1 minute
-      streamingETLQuery.awaitTermination(120000)
-
-
+      // Wait 2 minutes
+      streamingQuery.awaitTermination(120000)
   }
 }
 
